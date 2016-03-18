ca8c29062c0212e4c1fac4624e06ed3afb2a8c94
inoue_yuri <inoue_yuri@cyberagent.co.jp>
2016-03-18 13:04:02 +0900

    on the way.

--- language/i.py
+++ language/i.py
@@ -12,20 +12,34 @@ nw  = map( int, raw_input().split() )
 N = nw[0]
 W = nw[1]
 
-vws = [ map( int, raw_input().split()) for _ in xrange( N ) ]
+nws = [ map( int, raw_input().split()) for _ in xrange( N ) ]
 
-if N < 30:
-    val   = 0
-    total = 0
-    for i in xrange( N ):
-        V = vws[i][0]
-        W = vws[i][1]
-        for j in xrange( N ):
-        if total + W
-        val   += V
-        total += W
+max_value = 0
+ws = []
+#vs = []
+( wl, wr )  = ( 0, 0 )
+#( vl, vr )  = ( 0, 0 )
 
-        print vws
+for i in xrange( N ):
+    sub_value = 0
+    for j in xrange( N ):
+        if i == 0:
+            wr = 0
+        else:
+            wr = ws[i-1][j]
+        if j == 0:
+            wl = 0
+        else:
+            wl = ws[i][j-1]
+        if nws[j] + wr + wl > W:
+            break
+
+        ws[i][j] = nws[j][1] + wr + wl
+
+        sub_value += nws[j][0]
+
+    max_value = max( max_value, sub_value )
+
 
 #end = time.clock()
 #print( end - start )
--- tricky/a.py
+++ tricky/a.py
@@ -10,23 +10,18 @@ A - 整数割り算
 
 N  = int( raw_input() )
 
-ab = [ map( int, raw_input().split()) for _ in xrange( N ) ]
+ab = [ map( float, raw_input().split()) for _ in xrange( N ) ]
 
 res = []
 for x in ab:
     a = x[0]
     b = x[1]
     c = a / b
-    res.append( c )
-    # if c < 0:
-    #     if c > -1:
-    #         res.append( "0" )
-    #     else:
-    #         d = str( c )
-    #         pos = d.index(".")
-    #         res.append( d[0:pos] )
-    # else:
-    #     res.append( int( c ) )
+    if c > 0:
+        res.append( int( c ) )
+    else:
+        d = math.modf( c )
+        res.append( int(d[1]) )
 
 
 #end = time.clock()
